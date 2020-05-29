fd44d6014025160c3c68a45bfeb51e2c18b06855
Haibo Yang <6887993@qq.com>
2020-05-29 17:30:41 +0800

    Added manage user endpoint.

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework.test import APIClient
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**kwargs):
@@ -87,3 +88,50 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthenticated(self):
+        """Test that authentication is required for retrieving users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that requires authentication"""
+
+    def setUp(self) -> None:
+        self.user = create_user(
+            email='demo@demo.com',
+            password='123456',
+            name='test name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_user_successful(self):
+        """Test that retrieving user is successful"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_successful(self):
+        """
+        est that updating user profile for authenticated user is successful
+        """
+        payload = {'name': 'new name', 'password': 'newpass'}
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- user/urls.py
+++ user/urls.py
@@ -4,11 +4,12 @@ __date__ = '2020/5/29 10:13'
 
 from django.urls import path
 
-from user.views import CreateUserView, AuthTokenView
+from user.views import CreateUserView, AuthTokenView, ManageUserView
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', AuthTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
